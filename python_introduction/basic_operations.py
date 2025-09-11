dd53dc9b5d6ff4304f2314ff405d1ba9d022613a
Cecilia Grace <cecigrace2003@gmail.com>
2025-09-11 17:45:00 +0300

    Basic Arithmetic Exercise(+, -, *)

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -7,4 +7,4 @@ product = number1 * number2
 
 print(f'Sum of {number1} and {number2} is {sum}')
 print(f'Subtraction of {number1} and {number2} is {subtraction}')
-print(f'Product of {number1} and {number2} is {product}')
+print(f'Multiplication of {number1} and {number2} is {product}')
