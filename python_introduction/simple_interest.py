bcafaab773d5880a5e9f998d9c781d89d554fbf1
Cecilia Grace <cecigrace2003@gmail.com>
2025-09-11 17:59:16 +0300

    Simple Interest Calculator

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,7 +1,7 @@
-principal_dollars = 1000
-rate_percentage = 0.05
-time_years = 3
+principal = 1000
+rate = 0.05
+time = 3
 
-interest = principal_dollars * rate_percentage * time_years
+interest = principal * rate * time
 
 print(f'The simple interest is: {interest}')
