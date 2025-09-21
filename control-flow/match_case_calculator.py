c4d89dc9a46f3a9c12553626fdbe53be2a825c48
Cecilia Grace <cecigrace2003@gmail.com>
2025-09-21 15:51:14 +0300

    Calculates numbers according to user input(operator and numbers)

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,25 @@
+def calculator():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    operator = input("Choose the operation (+, -, *, /): ")
+
+    match operator:
+        case "+":
+            return f"The result is {num1 + num2}"
+        case "-":
+            return f"The result is {num1 - num2}"
+        case "*":
+            return f"The result is {num1 * num2}"
+        case "/":
+            if num2 != 0:
+                return f"The result is {num1 / num2}"
+            else:
+                print("Cannot divide by zero.")
+        case _:
+            return "Please enter correct operator"
+
+print(calculator())  
+
+
+  
