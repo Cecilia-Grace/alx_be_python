1b0e495e0b8cb440e88be3e39e89bba0d7044535
Cecilia Grace <cecigrace2003@gmail.com>
2025-09-22 13:49:21 +0300

    matchcase code update

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -2,9 +2,9 @@ def calculator():
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
 
-    operator = input("Choose the operation (+, -, *, /): ")
+    operation = input("Choose the operation (+, -, *, /): ")
 
-    match operator:
+    match operation:
         case "+":
             return f"The result is {num1 + num2}"
         case "-":
