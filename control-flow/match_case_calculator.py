06feafb86c3e40d16de5c7cebf59041ec0d95fcf
Cecilia Grace <cecigrace2003@gmail.com>
2025-09-22 13:58:31 +0300

    matchcase code update

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,25 +1,24 @@
-def calculator():
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
 
-    operation = input("Choose the operation (+, -, *, /): ")
+operation = input("Choose the operation (+, -, *, /): ")
 
-    match operation:
-        case "+":
-            return f"The result is {num1 + num2}"
-        case "-":
-            return f"The result is {num1 - num2}"
-        case "*":
-            return f"The result is {num1 * num2}"
-        case "/":
-            if num2 != 0:
-                return f"The result is {num1 / num2}"
-            else:
-                print("Cannot divide by zero.")
-        case _:
-            return "Please enter correct operator"
+match operation:
+    case "+":
+        print(f"The result is {num1 + num2}")
+    case "-":
+        print(f"The result is {num1 - num2}")
+    case "*":
+        print(f"The result is {num1 * num2}")
+    case "/":
+        if num2 != 0:
+            print(f"The result is {num1 / num2}")
+        else:
+            print("Cannot divide by zero.")
+    case _:
+        print("Please enter correct operator")
 
-print(calculator())  
+ 
 
 
   
