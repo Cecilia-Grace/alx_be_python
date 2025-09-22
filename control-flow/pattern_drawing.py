426b8f7eef376a9a098a688bfc49abf0ddc04a1d
Cecilia Grace <cecigrace2003@gmail.com>
2025-09-22 14:11:14 +0300

    pattern drawing update

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,14 +1,9 @@
-while True:
-    size_input = input("Enter the size of the pattern: ")
-    if size_input.isdigit() and int(size_input) > 0:
-        size = int(size_input)
-        break
-    else:
-        print("Invalid input. Please enter a positive integer.")
+size = int(input("Enter the size of the pattern: "))
 
 row = 0
+
 while row < size:
-    for _ in range(size):
+    for i in range(size):
         print("*", end="")
     
     print()  
