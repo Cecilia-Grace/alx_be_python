388f590f069778da1bd63c1fd70a6a2a4b501928
Cecilia Grace <cecigrace2003@gmail.com>
2025-09-28 21:57:08 +0300

    Shopping List Manager

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,34 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+
+    while True:
+        display_menu()
+
+        user_choice = input("Enter choice from the above: ")
+
+        if user_choice == "1":
+            added_item = input("Type the item you want to add to the shopping list: ")
+            shopping_list.append(added_item)
+            print(f"You have added: {added_item}")
+        elif user_choice == "2":
+            removed_item = input("Type the item you want to remove from the shopping list: ")
+            shopping_list.remove(removed_item)
+            print(f"You have removed: {removed_item}")
+        elif user_choice == "3":
+            print(f"Your shopping list has: {shopping_list}")
+        elif user_choice == "4":
+            print("Goodbye")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+            
+
+
+main()
