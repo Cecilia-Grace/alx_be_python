55c8715c442800d69b00b2b0113d7ceba0f9f9f7
Cecilia Grace <cecigrace2003@gmail.com>
2025-09-28 19:51:44 +0300

    Arithmetic Operations Function

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,20 @@
+def perform_operation(num1, num2, operation):
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        #to prevent division by zero
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Error: Cannot divide by zero"
+    else:
+        return "Error: Invalid operation specified"
+
+    
+    
+
+    
